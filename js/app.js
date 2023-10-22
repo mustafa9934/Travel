df08f473931ad1d5a966aa25047d68d275bdd68f
mustafa9934 <mustsfa.khan9434@gmail.com>
2023-10-22 22:53:17 +0200

    final project

---
+++ js/app.js
@@ -0,0 +1,256 @@
+var swiper = new Swiper(".mySwiper", {
+  pagination: {
+    el: ".swiper-pagination",
+    dynamicBullets: true,
+  },
+  autoplay: {
+    delay: 5000,
+  },
+});
+
+ScrollReveal().reveal(".nav", {
+  opacity: "0",
+});
+ScrollReveal().reveal(".header_main_title", {
+  distance: "20px",
+  opacity: "0",
+  origin: "bottom",
+  duration: 1000,
+});
+
+ScrollReveal().reveal(".header_form", {
+  distance: "20px",
+  opacity: "0",
+  origin: "bottom",
+  duration: 1000,
+  delay: 500,
+});
+
+ScrollReveal().reveal(".header_booked", {
+  distance: "20px",
+  opacity: "0",
+  origin: "bottom",
+  duration: 1000,
+  delay: 700,
+});
+
+ScrollReveal().reveal(".logo-1", {
+  distance: "20px",
+  opacity: "0",
+  origin: "bottom",
+  duration: 1000,
+  delay: 100,
+});
+ScrollReveal().reveal(".logo-2", {
+  distance: "20px",
+  opacity: "0",
+  origin: "bottom",
+  duration: 1000,
+  delay: 200,
+});
+ScrollReveal().reveal(".logo-3", {
+  distance: "20px",
+  opacity: "0",
+  origin: "bottom",
+  duration: 1000,
+  delay: 300,
+});
+ScrollReveal().reveal(".logo-4", {
+  distance: "20px",
+  opacity: "0",
+  origin: "bottom",
+  duration: 1000,
+  delay: 400,
+});
+ScrollReveal().reveal(".logo-5", {
+  distance: "20px",
+  opacity: "0",
+  origin: "bottom",
+  duration: 1000,
+  delay: 500,
+});
+
+ScrollReveal().reveal(".section_header_h4", {
+  distance: "20px",
+  opacity: "0",
+  origin: "bottom",
+  duration: 1000,
+});
+ScrollReveal().reveal(".section_header_h2", {
+  distance: "20px",
+  opacity: "0",
+  origin: "bottom",
+  duration: 1000,
+  delay: 100,
+});
+
+ScrollReveal().reveal(".offer-2", {
+  distance: "50px",
+  opacity: "0",
+  origin: "left",
+  duration: 1000,
+});
+
+ScrollReveal().reveal(".offer-3", {
+  distance: "50px",
+  opacity: "0",
+  origin: "right",
+  duration: 1000,
+});
+
+ScrollReveal().reveal(".offer-1", {
+  distance: "50px",
+  opacity: "0",
+  origin: "left",
+  duration: 1000,
+  delay: 100,
+});
+
+ScrollReveal().reveal(".offer-4", {
+  distance: "50px",
+  opacity: "0",
+  origin: "right",
+  duration: 1000,
+  delay: 100,
+});
+
+ScrollReveal().reveal(".feature_left", {
+  distance: "100px",
+  opacity: "0",
+  origin: "left",
+  duration: 1000,
+});
+
+ScrollReveal().reveal(".feature_right", {
+  distance: "100px",
+  opacity: "0",
+  origin: "right",
+  duration: 1000,
+});
+
+ScrollReveal().reveal(".holiday_title", {
+  distance: "100px",
+  opacity: "0",
+  origin: "bottom",
+  duration: 1000,
+});
+
+ScrollReveal().reveal(".tour_header_h4", {
+  distance: "40px",
+  opacity: "0",
+  origin: "bottom",
+  duration: 1000,
+});
+ScrollReveal().reveal(".tour_header_h2", {
+  distance: "40px",
+  opacity: "0",
+  origin: "bottom",
+  duration: 1000,
+  delay: 100,
+});
+ScrollReveal().reveal(".tour_header_p", {
+  distance: "40px",
+  opacity: "0",
+  origin: "bottom",
+  duration: 1000,
+  delay: 150,
+});
+
+ScrollReveal().reveal(".tour_header_btn", {
+  distance: "40px",
+  opacity: "0",
+  origin: "bottom",
+  duration: 1000,
+  delay: 200,
+});
+
+ScrollReveal().reveal(".tour_item_1", {
+  distance: "40px",
+  opacity: "0",
+  origin: "bottom",
+  duration: 1000,
+});
+ScrollReveal().reveal(".tour_item_2", {
+  distance: "40px",
+  opacity: "0",
+  origin: "bottom",
+  duration: 1000,
+  delay: 100,
+});
+ScrollReveal().reveal(".tour_item_3", {
+  distance: "40px",
+  opacity: "0",
+  origin: "bottom",
+  duration: 1000,
+  delay: 150,
+});
+ScrollReveal().reveal(".tour_item_4", {
+  distance: "40px",
+  opacity: "0",
+  origin: "bottom",
+  duration: 1000,
+  delay: 200,
+});
+ScrollReveal().reveal(".tour_item_5", {
+  distance: "40px",
+  opacity: "0",
+  origin: "bottom",
+  duration: 1000,
+  delay: 250,
+});
+
+ScrollReveal().reveal(".promotion_content", {
+  distance: "40px",
+  opacity: "0",
+  origin: "bottom",
+  duration: 1000,
+});
+
+ScrollReveal().reveal(".package_1", {
+  distance: "40px",
+  opacity: "0",
+  origin: "left",
+  duration: 1000,
+});
+
+ScrollReveal().reveal(".package_2", {
+  distance: "40px",
+  opacity: "0",
+  origin: "bottom",
+  duration: 1000,
+});
+
+ScrollReveal().reveal(".package_3", {
+  distance: "40px",
+  opacity: "0",
+  origin: "right",
+  duration: 1000,
+});
+
+ScrollReveal().reveal(".footer_item_1", {
+  distance: "40px",
+  opacity: "0",
+  origin: "bottom",
+  duration: 1000,
+});
+ScrollReveal().reveal(".footer_item_2", {
+  distance: "40px",
+  opacity: "0",
+  origin: "bottom",
+  duration: 1000,
+  delay: 100,
+});
+ScrollReveal().reveal(".footer_item_3", {
+  distance: "40px",
+  opacity: "0",
+  origin: "bottom",
+  duration: 1000,
+  delay: 150,
+});
+ScrollReveal().reveal(".footer_item_4", {
+  distance: "40px",
+  opacity: "0",
+  origin: "bottom",
+  duration: 1000,
+  delay: 200,
+});
